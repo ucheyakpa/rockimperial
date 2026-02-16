5f9af4b8aee91fe5ab20e46f7378032c49d5a9c3
Uchenna Akpa <ucheyakpa@gmail.com>
2026-02-16 02:26:48 +0100

    Innitial commit

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+// SIDEBAR
+const sidebar = document.querySelector(".sidebar");
+const show_sidebar = document.getElementById("show_sidebar");
+const close_sidebar = document.getElementById("close_sidebar");
+
+show_sidebar.addEventListener("click", function () {
+    sidebar.classList.add("active");
+})
+
+close_sidebar.addEventListener("click", function () {
+    sidebar.classList.remove("active");
+})
+
+// TOPBAR
+const menu = document.querySelector(".menu");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+
+open.addEventListener("click", function () {
+    menu.classList.add("show_menu");
+    open.style.display = "none";
+    close.style.display = "block";
+})
+
+close.addEventListener("click", function () {
+    menu.classList.remove("show_menu");
+    open.style.display = "block";
+    close.style.display = "none";
+})
+
+// PLAN SCRIPT
+const plans_list = document.querySelectorAll(".plans_title ul li");
+const plans_container = document.querySelectorAll(".plans_container");
+
+plans_container.forEach((container) => {
+    container.style.display = "none";
+})
+
+plans_container[0].style.display = "flex";
+
+plans_list.forEach((list, index) => {
+    list.addEventListener("click", () => {
+        plans_container.forEach((container) => {
+            container.style.display = "none";
+        })
+        plans_container[index].style.display = "flex";
+    })
+})
+
+
+
+
+
+
